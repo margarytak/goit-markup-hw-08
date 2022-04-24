05cbec3d916c7e799c0933419abc1a9593742159
Magraryta K <margaryta.kuryaninova@gmail.com>
2022-04-24 20:43:41 +0300

    first submission

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,43 @@
+// (() => {
+// const refs = {
+//     openMenuBtn: document.querySelector("[mob-menu-open]"),
+//     closeMenuBtn: document.querySelector("[mob-menu-close]"),
+//     menu: document.querySelector("[mob-menu]"),
+// };
+
+// refs.openMenuBtn.addEventListener("click", toggleMenu);
+// refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+// function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//     refs.menu.classList.toggle("is-open")
+// }
+// })();
+
 (() => {
-const refs = {
-    openMenuBtn: document.querySelector("[mob-menu-open]"),
-    closeMenuBtn: document.querySelector("[mob-menu-close]"),
-    menu: document.querySelector("[mob-menu]"),
-};
-
-refs.openMenuBtn.addEventListener("click", toggleMenu);
-refs.closeMenuBtn.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-}
-})();
+    const mobileMenu = document.querySelector('[mob-menu]');
+    const openMenuBtn = document.querySelector('[mob-menu-open]');
+const closeMenuBtn = document.querySelector('[mob-menu-close]');
+  
+    const toggleMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
+  
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+  
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
